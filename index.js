30229f71379244cb8a69c9dc7123a92f2f78cefb
yuval miles <yuvalmiles@gmail.com>
2022-06-19 15:46:45 +0300

    milestone_5

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const xElem = document.querySelector("#x");
 const button = document.querySelector("#button");
 const yElem = document.querySelector("#y");
+const spinner = document.querySelector("#spinner");
+const lgError = document.querySelector("#ln-error");
+
+const toggleLoad = () => {
+  yElem.classList.toggle("d-none");
+  spinner.classList.toggle("d-none");
+};
+
+const toggleLargeNumError = async () => {
+  lgError.classList.toggle("d-none");
+  setTimeout(() => {
+    lgError.classList.toggle("d-none");
+  }, 3000);
+};
 
 button.addEventListener("click", async () => {
-  const rs = await fetch(`http://localhost:5050/fibonacci/${xElem.value}`);
-  const { result } = await rs.json();
-  yElem.innerText = result;
+  if (xElem.value > 50) return toggleLargeNumError();
+  try {
+    toggleLoad();
+    const rs = await fetch(`http://localhost:5050/fibonacci/${xElem.value}`);
+    if (!rs.ok) throw new Error(await rs.text());
+    const { result } = await rs.json();
+    toggleLoad();
+    yElem.innerText = result;
+  } catch (err) {
+    yElem.innerHTML = `<span style="color:red;">Server Error: ${err.message}<span>`;
+    toggleLoad();
+  }
 });
