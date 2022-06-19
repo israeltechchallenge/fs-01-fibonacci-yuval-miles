fc8bf59499e0ee72725ca8eb75b274b203ebf5d1
yuval miles <yuvalmiles@gmail.com>
2022-06-19 14:44:31 +0300

    milestone_2

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const xElem = document.querySelector("#x");
 const yElem = document.querySelector("#y");
 
-xElem.innerHTML = `&nbsp${5}&nbsp`;
-yElem.innerHTML = `&nbsp${3}&nbsp`;
+const getFibAtIndex = (x) => {
+  if (x < 0) return;
+  const fib = [0, 1];
+  if (!x) return 0;
+  for (let i = 2; i < x; i++) {
+    fib.push(fib[0] + fib[1]);
+    fib.shift();
+  }
+  return fib[1];
+};
+
+xElem.innerHTML = `&nbsp${9}&nbsp`;
+yElem.innerHTML = `&nbsp${getFibAtIndex(9)}&nbsp`;
