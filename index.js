a1ab7511c040af9179af94f9a7f78c710a4fca6d
yuval miles <yuvalmiles@gmail.com>
2022-06-19 16:22:06 +0300

    fixed function

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const getFibAtIndex = (x) => {
   if (x < 0) return;
   const fib = [0, 1];
   if (!x) return 0;
-  for (let i = 2; i < x; i++) {
+  for (let i = 1; i < x; i++) {
     fib.push(fib[0] + fib[1]);
     fib.shift();
   }
   return fib[1];
 };
 
-xElem.innerHTML = `&nbsp${9}&nbsp`;
-yElem.innerHTML = `&nbsp${getFibAtIndex(9)}&nbsp`;
+xElem.innerHTML = `&nbsp${3}&nbsp`;
+yElem.innerHTML = `&nbsp${getFibAtIndex(3)}&nbsp`;
