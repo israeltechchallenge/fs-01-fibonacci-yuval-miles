d512246352c44496dd42c22d8702cdac9d44f3e2
yuval miles <yuvalmiles@gmail.com>
2022-06-20 09:25:56 +0300

    set restrictions on input

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const button = document.querySelector("#button");
 const yElem = document.querySelector("#y");
 
 const getFibAtIndex = (x) => {
-  if (x < 0) return;
   const fib = [0, 1];
   if (!x) return 0;
   for (let i = 1; i < x; i++) {
